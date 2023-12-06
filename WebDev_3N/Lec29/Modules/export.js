8290b3d7fc0775d57876ba30d56953ac782c9fe0
Sachin <sachinongithub@gmail.com>
2023-12-06 10:19:30 +0530

    Code updated on 6th Dec

---
+++ WebDev_3N/Lec29/Modules/export.js
@@ -0,0 +1,12 @@
+// export let firstName = "Sachin";
+// export let lastName = "Barpanda";
+// let val = 99;
+// export default 99; 
+
+let year = 2025;
+let gameRelease = "GTA6";
+let rohitPlaced = "Google";
+
+export{
+    year,gameRelease,rohitPlaced
+}
---
+++ WebDev_3N/Lec29/Modules/import.js
@@ -0,0 +1,17 @@
+// import { firstName,lastName } from "./export.js";
+
+// console.log(firstName);
+// console.log(lastName);
+
+// import { firstName as first} from "./export.js";
+
+// console.log(first);
+
+// import teaBag from "./export.js"
+// console.log(teaBag);
+
+import {
+    year,gameRelease,rohitPlaced
+} from './export.js'
+
+console.log(`In the year ${year} the game ${gameRelease} will be released and Rohit will be placed in ${rohitPlaced}`);
